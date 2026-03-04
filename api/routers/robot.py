331149a8a45d997a61a7a32464f8eda0d37be9b4
JAVIER CORDOVA <xavier170791@gmail.com>
2026-03-04 01:07:14 -0500

    feat #26 primer redis

--- api/routers/robot.py
+++ api/routers/robot.py
@@ -1,6 +1,10 @@
 from fastapi import APIRouter, UploadFile, File, Form, Depends
 from fastapi.responses  import JSONResponse
 from api.dependencies import getCurrentUser, getDni, getIaResponse
+from infrastructure.workers.tasks.ocrTask import process_ocr
+from celery.result import AsyncResult
+from infrastructure.workers.celeryApp import celery_app
+from infrastructure.workers.tasks.ocrTask import test_task
 import json
 
 routerRobot = APIRouter(prefix='/Robot', tags=['ROBOT'])
@@ -41,3 +45,38 @@ async def getResponseIa(
     response['status']  =   200  
     response['data']    =   await useCase.getDataFile(fileBytes, *feature)
     return JSONResponse(status_code=200, content=response)
+
+
+@routerRobot.post("/robot/ocrGeminiCelery")
+async def getResponseIaCelery(
+    feature :   list[str] = Form(...),
+    file    :   UploadFile | None = File(None),
+    name    =   Depends(getCurrentUser) 
+):
+    fileBytes   = await file.read()
+    task        = process_ocr.delay(fileBytes, feature)
+    return {
+        "status": "processing",
+        "task_id": task.id,
+        "result_url": f"/robot/result/{task.id}"
+    }
+
+@routerRobot.get("/robot/result/{task_id}")
+async def get_result(task_id: str):
+
+    result = AsyncResult(task_id, app=celery_app)
+
+    if result.ready():
+        return {
+            "status": "completed",
+            "data": result.result
+        }
+
+    return {"status": "processing"}
+
+@routerRobot.get("/testCelery")
+async def testCelery():
+
+    task = celery_app.send_task("tasks.test_task", args=[4,6])
+
+    return {"task_id": task.id}
